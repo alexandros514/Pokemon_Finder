d0426f02d03e7d118a4ab575cd3845ce236f85b9
alexandros514 <66693474+alexandros514@users.noreply.github.com>
2025-02-05 10:18:38 +0200

    css / cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,8 +10,8 @@ class HomeController < ApplicationController
       @pokemon_image = @pokemon["sprites"]["front_default"]
       @pokemon_image_shiny = @pokemon["sprites"]["front_shiny"]
       @pokemon_name = @pokemon["name"]
-      @pokemon_ability = @pokemon["abilities"].map{|ability_hash| ability_hash["ability"]["name"]}.join(" & ")
-      @pokemon_types = @pokemon["types"].map{|type_hash| type_hash["type"]["name"]}.join(" & ") #[0]["type"]["name"]
+      @pokemon_ability = @pokemon["abilities"].map { |ability_hash| ability_hash["ability"]["name"] }.join(" & ")
+      @pokemon_types = @pokemon["types"].map { |type_hash| type_hash["type"]["name"] }.join(" & ") # [0]["type"]["name"]
       @pokemon_exp = @pokemon["base_experience"]
       @pokemon_weight =@pokemon["weight"]
 
@@ -19,12 +19,10 @@ class HomeController < ApplicationController
     resp_tcg = HTTParty.get("https://api.pokemontcg.io/v2/cards?q=!name:#{search_tcg}")
       if resp_tcg.code != 404
         @pokemon_tcg = resp_tcg
-        @pokemon_tcg_images = @pokemon_tcg["data"].map{|tcgplayer_hash| tcgplayer_hash["images"]["small"]}
+        @pokemon_tcg_images = @pokemon_tcg["data"].map { |tcgplayer_hash| tcgplayer_hash["images"]["small"] }
         @pokemon_tcg_images_core =@pokemon_tcg["data"].first["images"]["large"]
         @pokemon_tcg_txt = @pokemon_tcg["data"].first["flavorText"]
       end
-
     end
-
   end
 end
