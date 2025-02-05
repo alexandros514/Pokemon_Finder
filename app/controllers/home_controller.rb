7b1a12222f294d769f9c87422c82dd9a1a74fa54
alexandros514 <66693474+alexandros514@users.noreply.github.com>
2025-02-05 11:57:01 +0200

    validate user input

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
     search_for_value = params[:q]
-    if search_for_value.present?
-      resp = HTTParty.get("https://pokeapi.co/api/v2/pokemon/#{search_for_value}/")
+    if search_for_value.present? && search_for_value.length < 20
+      encoded_search = URI.encode_uri_component(search_for_value.strip.gsub(/\s/, "-"))
+      resp = HTTParty.get("https://pokeapi.co/api/v2/pokemon/#{encoded_search}/")
       if resp.code == 404
-        return  redirect_to root_path
+        return redirect_to root_path
       end
       @pokemon = resp
       @pokemon_image = @pokemon["sprites"]["front_default"]
@@ -13,14 +14,20 @@ class HomeController < ApplicationController
       @pokemon_ability = @pokemon["abilities"].map { |ability_hash| ability_hash["ability"]["name"] }.join(" & ")
       @pokemon_types = @pokemon["types"].map { |type_hash| type_hash["type"]["name"] }.join(" & ") # [0]["type"]["name"]
       @pokemon_exp = @pokemon["base_experience"]
-      @pokemon_weight =@pokemon["weight"]
+      @pokemon_weight = @pokemon["weight"]
 
-    search_tcg = @pokemon_name
-    resp_tcg = HTTParty.get("https://api.pokemontcg.io/v2/cards?q=!name:#{search_tcg}")
+      search_tcg = @pokemon_name
+      resp_tcg = HTTParty.get("https://api.pokemontcg.io/v2/cards?q=!name:#{search_tcg}")
       if resp_tcg.code != 404
         @pokemon_tcg = resp_tcg
+
+        if @pokemon_tcg["data"].blank?
+          @pokemon_tcg = nil
+          return
+        end
+
         @pokemon_tcg_images = @pokemon_tcg["data"].map { |tcgplayer_hash| tcgplayer_hash["images"]["small"] }
-        @pokemon_tcg_images_core =@pokemon_tcg["data"].first["images"]["large"]
+        @pokemon_tcg_images_core = @pokemon_tcg["data"].first["images"]["large"]
         @pokemon_tcg_txt = @pokemon_tcg["data"].first["flavorText"]
       end
     end
